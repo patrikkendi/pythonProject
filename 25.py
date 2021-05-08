59cb89bc099e8497a7b7208f0853a357619b6a24
patrikkendi <thepatrick4125@gmail.com>
2021-05-08 16:44:06 +0200

    1 mentés

--- 25.py
+++ 25.py
@@ -13,7 +13,7 @@ rng[0] = int(rng[0])
 rng[1] = int(rng[1])
 
 #Létrehozom a véletlenszerű műveleti jeleket
-def randomOperato():
+def randomOperator():
     operators = ['+','-','*','/']
     op = random.choice(operators)
     return op
@@ -29,4 +29,30 @@ def expressionMake(v):
                 else:
                     prime_numbers.append(v[i])
 
+    #Megnézem hogy a szám páros e vagy páratlan
     for i in range(len(v)):
+        if (v[i] % 2) == 0:
+            even_numbers.append(v[i])
+        else:
+            odd_numbers.append(v[i])
+
+    if len(prime_numbers) > 1:
+        return 'prime_numbers', prime_numbers
+    elif len(odd_numbers) > 1:
+        return 'odd_numbers', odd_numbers
+    else:
+        return 'even_numbers', even_numbers
+
+#Létrehozom azt a függvényt,
+#mely a kapott listán végig iterál, hozzáad egy random operátort,
+#majd megoldja a kifejezést
+def expressionSolve(iterable):
+
+    expression = ''
+    for i in range(len(iterable)):
+        operation_string = randomOperator()
+        if i == len(iterable)-1: #Mivel az utolsó után nemkell operátor
+            expression += f'{iterable[i]}'
+        else:
+            expression += f'{iterable[i]} {operation_string}'
+
